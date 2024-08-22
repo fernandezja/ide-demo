6aa4ddd3fc21b3e55694eae8ef52008bc93eebe5
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2024-08-22 18:07:23 -0300

    demo c#

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var name = "Hello World";
+Console.WriteLine($"{name} {Calc(2, 3)}");
+
+int Calc(int a, int b){
+    return a*b;
+}
